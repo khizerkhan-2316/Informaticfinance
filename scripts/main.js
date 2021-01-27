32045033b58872dcd581226406fbfdf82321d9ed
Khizer Khan <khizer759@gmail.com>
2021-01-27 16:00:50 +0100

    Made the responsive nav-bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
 console.log("Script is running");
 //Import of components/Modules
+
+// Js for hamburger menu:
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}

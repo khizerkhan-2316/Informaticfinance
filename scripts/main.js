b7bd8e937512166a16e7729bd8908ba838ea7ac4
Khizer Khan <khizer759@gmail.com>
2021-01-27 22:12:55 +0100

    Navigation and first part of main-section almost done

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,24 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+// getting API data:
+const apiKey = "84bee8e9-32a5-4e6f-b0fe-311ddc4c69e6";
+
+const options = {
+  method: "GET",
+  url: "https://coingecko.p.rapidapi.com/coins/bitcoin/tickers",
+  headers: {
+    "x-rapidapi-key": "1cc4df03e6msh9528fc3e887dbbbp10fb68jsn9ea9a023f3e9",
+    "x-rapidapi-host": "coingecko.p.rapidapi.com",
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data.tickers);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });

6570be8e55cdd0d125b25e2ba75a055ddbe6be3e
Khizer Khan <khizer759@gmail.com>
2021-01-29 22:20:00 +0100

    Added almost all functionality

--- scripts/main.js
+++ scripts/main.js
@@ -119,3 +119,19 @@ setInterval(function () {
 
   marketvalueChange();
 }, 60000);
+const ctx = document.getElementById("myChart");
+var myLineChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+    datasets: [
+      {
+        data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478, 4000],
+        label: "BTC",
+        borderColor: "#3e95cd",
+        fill: false,
+      },
+    ],
+  },
+  options: options,
+});

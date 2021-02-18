bb9d08a924fe84e8d8cd924cadb513155e8608fc
Khizer Khan <khizer759@gmail.com>
2021-02-18 13:40:34 +0100

    Fixed responsivness issues

--- scripts/main.js
+++ scripts/main.js
@@ -119,16 +119,17 @@ setInterval(function () {
 
   marketvalueChange();
 }, 60000);
+
 const ctx = document.getElementById("myChart");
 var myLineChart = new Chart(ctx, {
   type: "line",
   data: {
-    labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+    labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950],
     datasets: [
       {
-        data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478, 4000],
+        data: [86, 114, 106, 106, 107, 111, 133, 221],
         label: "BTC",
-        borderColor: "#3e95cd",
+        borderColor: "#FFF",
         fill: false,
       },
     ],
